e5af63057133657c6f6bd96d1c0242fea1bb143c
Terry O'Shea <terrykoshea@gmail.com>
2016-10-21 02:52:14 +0000

    sdf

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth': {
         'clientID': process.env.FB_APP_ID, 
         'clientSecret': process.env.FB_APP_SECRET, 
-        'callbackURL': 'https://voteroo.herokuapp.com/auth/facebook/callback'
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback'
     }, 
     'twitterAuth': {
         'callbackURL': 'https://voteroo.herokuapp.com/auth/twitter/callback'
