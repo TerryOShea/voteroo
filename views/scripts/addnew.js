08a51ae6746d401ae8a7b6ff97a709f7a890cb5a
Terry O'Shea <terrykoshea@gmail.com>
2016-11-21 06:23:01 +0000

    style password reset pages

--- views/scripts/addnew.js
+++ views/scripts/addnew.js
@@ -1,20 +1,29 @@
 var addOptionBtn = document.querySelector('.add-option-btn'), 
     addnewOptionsBox = document.querySelector('.addnew-options-box'), 
-    counter = 3;
+    removeOptionBtn = document.querySelector('.remove-option-btn'), 
+    counter = 2;
     
 function addOption() {
-    let input = document.createElement('input');
-    input.type = 'text';
-    input.className = 'addnew-option';
-    input.name = 'options';
-    input.placeholder = 'Option ' + counter;
-    input.maxlength = '30';
-    addnewOptionsBox.appendChild(input);
-    counter += 1;
+    if (counter < 10) {
+        counter += 1;
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'addnew-option';
+        input.name = 'options';
+        input.placeholder = 'Option ' + counter;
+        input.id = 'option-' + counter;
+        input.maxlength = '30';
+        addnewOptionsBox.appendChild(input);
+    }
+}
+
+function removeOption() {
+    if (counter > 2) {
+        let option = document.getElementById('option-' + counter);
+        option.parentElement.removeChild(option);
+        counter -= 1;
+    }
 }
 
 addOptionBtn.addEventListener('click', addOption);
-
-if (counter == 10) {
-    addOptionBtn.removeEventListener('click', addOption);
-}
+removeOptionBtn.addEventListener('click', removeOption);
